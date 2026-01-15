ef046d95e8e82d94328638615c5328be4c419dc9
Samin-Sadaf7 <saminsadaf@iut-dhaka.edu>
2026-01-15 22:03:27 +0600

    Updated inference.py

--- inference.py
+++ inference.py
@@ -1,8 +1,12 @@
 import torch
+
 def predict(model, imgs):
+    device = next(model.parameters()).device
     model.eval()
+
     with torch.no_grad():
-        p1, p2 = model(imgs.cuda())
+        imgs = imgs.to(device)
+        p1, p2 = model(imgs)
         d1 = p1.argmax(dim=1)
         d2 = p2.argmax(dim=1)
         return d1 * 10 + d2
