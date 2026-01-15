9c2b2dbcff6d650de3930b9a0e181e0e7bae79ef
Samin-Sadaf7 <saminsadaf@iut-dhaka.edu>
2026-01-15 22:41:45 +0600

    Added validation

--- validate.py
+++ validate.py
@@ -1,11 +1,11 @@
 import torch
-from sklearn.metrics import accuracy_score, precision_recall_fscore_support
+from sklearn.metrics import accuracy_score
 
 def validate(model, dataloader, device):
     model.eval()
 
-    y1_true, y1_pred = [], []
-    y2_true, y2_pred = [], []
+    d1_true, d1_pred = [], []
+    d2_true, d2_pred = [], []
 
     with torch.no_grad():
         for imgs, d1, d2 in dataloader:
@@ -18,31 +18,17 @@ def validate(model, dataloader, device):
             p1 = torch.argmax(o1, dim=1)
             p2 = torch.argmax(o2, dim=1)
 
-            y1_true.extend(d1.cpu().numpy())
-            y1_pred.extend(p1.cpu().numpy())
+            d1_true.extend(d1.cpu().numpy())
+            d1_pred.extend(p1.cpu().numpy())
+            d2_true.extend(d2.cpu().numpy())
+            d2_pred.extend(p2.cpu().numpy())
 
-            y2_true.extend(d2.cpu().numpy())
-            y2_pred.extend(p2.cpu().numpy())
-
-    acc1 = accuracy_score(y1_true, y1_pred)
-    acc2 = accuracy_score(y2_true, y2_pred)
-
-    f1_1 = precision_recall_fscore_support(
-        y1_true, y1_pred, average="macro"
-    )[2]
-    f1_2 = precision_recall_fscore_support(
-        y2_true, y2_pred, average="macro"
-    )[2]
+    acc1 = accuracy_score(d1_true, d1_pred)
+    acc2 = accuracy_score(d2_true, d2_pred)
 
     jersey_acc = sum(
         (a == b and c == d)
-        for a, b, c, d in zip(y1_true, y1_pred, y2_true, y2_pred)
-    ) / len(y1_true)
+        for a, b, c, d in zip(d1_true, d1_pred, d2_true, d2_pred)
+    ) / len(d1_true)
 
-    return {
-        "Digit1 Acc": acc1,
-        "Digit2 Acc": acc2,
-        "Digit1 F1": f1_1,
-        "Digit2 F1": f1_2,
-        "Jersey Acc": jersey_acc
-    }
+    return acc1, acc2, jersey_acc
